e25c8f186472a6e33eb4ce6df9343bb6f7cfe0d0
effprime <exdoubledot@gmail.com>
2020-03-15 16:21:43 -0400

    Clean up

--- internal/router/endpoints/posts.go
+++ internal/router/endpoints/posts.go
@@ -1,6 +1,7 @@
 package endpoints
 
 import (
+	"database/sql"
 	"encoding/json"
 	"goblog/internal/database"
 	"goblog/internal/utils"
@@ -9,37 +10,56 @@ import (
 )
 
 type Post struct {
-	Title         string `json:"title"`
-	Posted        time.Time
-	Posted_string string `json:"posted"`
-	Body          string `json:"body"`
+	Title  string    `json:"title"`
+	Posted time.Time `json:"posted"`
+	Body   string    `json:"body"`
+	Id     int       `json:"id"`
 }
 
 func NewPostHandler(w http.ResponseWriter, r *http.Request) {
 	var post Post
 	utils.GetPostRequestData(r, &post)
 	post.Posted = time.Now()
-	//todo ... DB stuff
+
+	db := database.GetDatabase()
+	sqlStatement := `
+	INSERT INTO public.post (title, posted, body)
+	VALUES ($1, $2, $3)`
+	_, err := db.Exec(sqlStatement, post.Title, post.Posted, post.Body)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func AllPostsHandler(w http.ResponseWriter, r *http.Request) {
+	limit, ok := r.URL.Query()["limit"]
+	if !ok || len(limit[0]) < 1 {
+		limit = nil
+	}
+
+	var result *sql.Rows
+	var err error
 	db := database.GetDatabase()
-	result, err := db.Query("SELECT * FROM public.post")
+	if limit != nil {
+		result, err = db.Query("SELECT * FROM public.post LIMIT $1", limit[0])
+	} else {
+		result, err = db.Query("SELECT * FROM public.post")
+	}
 	if err != nil {
 		panic(err)
-	} else {
-		defer result.Close()
-
-		posts := []Post{}
-		for result.Next() {
-			var post Post
-			err := result.Scan(&post.Title, &post.Posted_string, &post.Body)
-			if err != nil {
-				panic(err.Error())
-			}
-			posts = append(posts, post)
-		}
-		json.NewEncoder(w).Encode(posts)
 	}
+	defer result.Close()
+
+	posts := []Post{}
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.Title, &post.Posted, &post.Body, &post.Id)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+	json.NewEncoder(w).Encode(posts)
 
 }
